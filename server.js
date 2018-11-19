28b34842863685b96b91f4493b19c86d98751e83
Balkrishna Meena <balkrishna.meena@in.ey.com>
2018-11-19 15:58:09 +0530

    deployment test

--- server.js
+++ server.js
@@ -2,19 +2,7 @@ const path = require('path');
 const express = require('express');
 const app = express();
 
-// Serve static files
-app.use(express.static(__dirname + '/dist/fitTreat-adminapp'));
-
-// Send all requests to index.html
-app.get('/*', function(req, res) {
-  res.sendFile(path.join(__dirname + '/dist/fitTreat-adminapp/index.html'));
-});
-
-// default Heroku port
-
 let env = process.env.NODE_ENV || 'development';
-
-app.listen(process.env.PORT || 5000);
 
 let forceSSL = (req, res, next) => {
   if (req.headers['x-forwarded-proto'] !== 'https') {
@@ -26,3 +14,27 @@ let forceSSL = (req, res, next) => {
 if (env === 'production') {
   app.use(forceSSL);
 }
+
+// Serve static files
+app.use(express.static(__dirname + '/dist/fitTreat-adminapp'));
+
+const allowedExt = [
+  '.js',
+  '.ico',
+  '.css',
+  '.png',
+  '.jpg',
+  '.woff2',
+  '.woff',
+  '.ttf',
+  '.svg',
+];
+
+// Send all requests to index.html
+app.get('/*', function(req, res) {
+  res.sendFile(path.join(__dirname + '/dist/fitTreat-adminapp/index.html'));
+});
+
+// default Heroku port
+app.listen(process.env.PORT || 5000);
+
