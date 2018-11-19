f2b165109a87412cfa43c55652b22e164d9d92ed
Balkrishna Meena <balkrishna.meena@in.ey.com>
2018-11-19 15:25:13 +0530

    angular app deploy - test

--- src/app/app-constants.ts
+++ src/app/app-constants.ts
@@ -1,8 +1,8 @@
 export const AppConstants:any = {
     //local setup
-    baseUrl : "http://localhost:8888"
+    //baseUrl : "http://localhost:8888"
     //cloud url
     //baseUrl : "https://fittreat.cfapps.us10.hana.ondemand.com"
     //heroku url
-    //baseUrl : "https://fit-treat-server.herokuapp.com"
+    baseUrl : "https://fit-treat-server.herokuapp.com"
 }
