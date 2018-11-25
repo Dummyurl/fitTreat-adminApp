367a0c6d2506e1d84bd2e8874291e3ec385d40fa
Balkrishna Meena <balkrishna.meena@in.ey.com>
2018-11-26 00:57:51 +0530

    base url changed- cloud version

--- src/app/app-constants.ts
+++ src/app/app-constants.ts
@@ -4,5 +4,5 @@ export const AppConstants:any = {
     //cloud url
     //baseUrl : "https://fittreat.cfapps.us10.hana.ondemand.com"
     //heroku url
-    baseUrl : "https://fit-treat-server.herokuapp.com"
+    baseUrl : "https://fittreat-server.herokuapp.com"
 }
