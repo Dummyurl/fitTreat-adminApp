d0d1b38d5719093b4a46d058d40d802edb667a33
Balkrishna Meena <balkrishna.meena@in.ey.com>
2018-11-25 23:05:05 +0530

    changes

--- src/app/app-constants.ts
+++ src/app/app-constants.ts
@@ -1,6 +1,6 @@
 export const AppConstants:any = {
     //local setup
-    //baseUrl : "http://localhost:8888"
+    //baseUrl : "http://localhost:8888" //for local
     //cloud url
     //baseUrl : "https://fittreat.cfapps.us10.hana.ondemand.com"
     //heroku url
