55381f7dadf556b43bf30383ca196906440ed6eb
Balkrishna Meena <balkrishna.meena@in.ey.com>
2019-01-06 02:11:41 +0530

    configuration var for backend url

--- src/app/app-constants.ts
+++ src/app/app-constants.ts
@@ -1,8 +1,3 @@
 export const AppConstants: any = {
-    // local setup
-     baseUrl : 'http://localhost:8888' // for local
-    // heroku url
-    // baseUrl : "https://fittreat-server.herokuapp.com"
-    // heroku python
-    // baseUrl: 'https://fittreat-python.herokuapp.com'
+     baseUrl : process.env.BACKEND_URL || 'http://localhost:8888' // for local
 };
